e3185a4feb2162969112ae0cc07b3fa72493d70b
ValeriGuerrero <valeriguerrero22@outlook.com>
2024-08-19 14:36:37 -0500

    correct lesson 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,47 @@ import TodoList from './TodoList.jsx'
 import TodoListItem from './TodoListItem';
 
 
+
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  /*console.log(import.meta.env)*/
 
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const existingList = JSON.parse(localStorage.getItem('savedTodoList')) || [];
-        const object = {
-          data: {
-            todoList: existingList,
-          },
-        };
-        resolve(object);
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    })
+  const fetchData = async () => {
+    const options = {}
+    options.method = 'GET';
+    options.headers = {
+      Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+    }
+    const url =
+      `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
+    /*console.log('url', url)*/
+    try {
+      const response = await fetch(url, options)
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json()
+      console.log(data)
+
+      const todos = data.records.map((todo) => {
+        return {
+          id: todo.id,
+          title: todo.fields.title
+        }
+      })
+      console.log(todos)
+
+      setTodoList(todos)
+      setIsLoading(false)
+    } catch (error) {
+      console.log("Error", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   useEffect(() => {
